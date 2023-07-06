d4c40b429fcd9a47adc1b4b8ed1cb988a9f7bf4c
jianghuxinyi <jianghuxinyi@gmail.com>
2023-07-06 23:51:41 +0800

    feat(Conversation List): Conversation列表添加展开收起行数变化和对应背景颜色变化

--- chat/src/main/java/com/example/chat/MainActivity.kt
+++ chat/src/main/java/com/example/chat/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.chat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,8 +25,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,10 +58,10 @@ class MainActivity : ComponentActivity() {
 data class Message(val author: String, val body: String)
 
 @Composable
-fun Conversation(messages: List<Message>){
-    LazyColumn(){
-        items(messages){
-            item: Message ->  MessageCard(msg = item)
+fun Conversation(messages: List<Message>) {
+    LazyColumn() {
+        items(messages) { item: Message ->
+            MessageCard(msg = item)
         }
 
     }
@@ -68,23 +76,37 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(42.dp)
                 .clip(CircleShape)
-                .background(color = MaterialTheme.colorScheme.secondary)
+                .border(1.5.dp,MaterialTheme.colorScheme.secondary, CircleShape)
+                .background(color = MaterialTheme.colorScheme.inverseSurface)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column() {
+        var isExpanded by remember { mutableStateOf(false) }
+
+        val surfaceColor by animateColorAsState(
+            targetValue = if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+                )
+
+        Column(
+            modifier = Modifier.clickable { isExpanded = !isExpanded }
+        ) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.headlineMedium
-                )
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            Surface(shape = RoundedCornerShape(4.dp)) {
+            Surface(
+                shape = RoundedCornerShape(4.dp),
+                shadowElevation = 1.dp,
+                color = surfaceColor
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(4.dp),
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
                 )
             }
         }
@@ -94,7 +116,7 @@ fun MessageCard(msg: Message) {
 
 @Preview
 @Composable
-fun PreviewConversation(){
+fun PreviewConversation() {
     Conversation(SampleData.conversationSample)
 }
 
